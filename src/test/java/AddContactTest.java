b6724f34c9b22045e5caa69366f0e21921a03569
AntonStebelev <astebelev@gmail.com>
2021-05-27 16:53:26 +0300

    added lombok library

--- src/test/java/AddContactTest.java
+++ src/test/java/AddContactTest.java
@@ -1,9 +1,9 @@
 import models.Contact;
-        import org.openqa.selenium.By;
-        import org.testng.Assert;
-        import org.testng.annotations.AfterMethod;
-        import org.testng.annotations.BeforeMethod;
-        import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class AddContactTest extends TestBase{
 
